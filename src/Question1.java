3e9fa3a3cbb9e563b0a69c3359dc3937f073b9d0
tewan2657 <tewan2657@HRH-IC0027326.wrdsb.ca>
2017-09-29 10:11:50 -0400

    Working on question 1 and 2

--- src/Question1.java
+++ src/Question1.java
@@ -12,7 +12,42 @@ public class Question1 {
     /**
      * @param args the command line arguments
      */
+    public void swap(int[] array, int p1, int p2) {
+        int temp = array[p1];
+        array[p1] = array[p2];
+        array[p2] = temp;
+    }
+
+    public void Sorting(int[] array) {
+        //keep track of of which positions we are sorting 
+        for (int position = 0; position < array.length; position++) {
+            //go through the numbers looking for the smallest number
+            for (int j = position + 1; j < array.length; j++) {
+                //is this the smallest number
+                if (array[j] < array[position]) {
+                    
+                    swap(array, j, position);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Question1 test = new Question1();
+        
+        //Array of Numbers
+        int[] array = {12, 65, 18, 59, 78, 89, 2, 33};
+
+        System.out.println("BEFORE");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        //sorting
+        test.Sorting(array);
+
+         System.out.println("AFTER");
+         for (int i = 0; i < array.length; i++) {
+             System.out.println(array[i]);
+        }
     }
 }
---
+++ src/Question2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tewan2657
+ */
+public class Question2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void Sorting(int[]array){
+        for (int position = 0; position < array.length; position++) {
+            for (int j = position + 1; j < array.length; j++) {
+                if(array[j] < array[position]){
+                    
+                  int temp = array[position];
+                  array[position] = array[j];
+                  array[j] = temp;
+                }
+            }
+        }
+   
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) { 
+        Question2 test = new Question2();
+        
+        int[] array = {7,1,2,3,5,6,7};
+        
+        test.Sorting(array);
+        
+        System.out.println("SORTED");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        
+    }
+}
