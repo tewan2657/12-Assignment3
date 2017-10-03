425e9317519168c5f1df816234ecfeb7ccc85c22
tewan2657 <tewan2657@HRH-IC0027326.wrdsb.ca>
2017-10-03 10:06:30 -0400

    finished question 3

--- src/Question2.java
+++ src/Question2.java
@@ -13,10 +13,13 @@ public class Question2 {
      * @param args the command line arguments
      */
     public void Sorting(int[]array){
+        //keeping track of the position 
         for (int position = 0; position < array.length; position++) {
+            // go through the rest of the array to look for small numbers
             for (int j = position + 1; j < array.length; j++) {
+                //is this the smallest number
                 if(array[j] < array[position]){
-                    
+                   //swap 
                   int temp = array[position];
                   array[position] = array[j];
                   array[j] = temp;
@@ -25,23 +28,37 @@ public class Question2 {
         }
    
     }
+   public int smallestMissingNumbers(int[] array){
+
+       //go through the array
+       for (int i = 0; i < array.length; i++) {
+     
+             
+         } 
+       
+       
+       
+       return -1;
+   }
     
     
     
-    
-    
-    
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        //testing 
         Question2 test = new Question2();
-        
+        //array 
         int[] array = {0,1,2,3,5,6,7};
-        
+        //sorting method 
         test.Sorting(array);
-        
+        //print sorted array 
         System.out.println("SORTED");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
+        
+        
+        
+        
         
     }
 }
--- src/Question3.java
+++ src/Question3.java
@@ -12,34 +12,61 @@ public class Question3 {
     /**
      * @param args the command line arguments
      */
-    public int binarySearch (int[]array, int target){
-     int min =0;
-     int max = array.length - 1;
-     while(max > min){
-         int guess = (max + min) /2;
-         //find it??
-         if(array[guess] == target){
-             return guess;
-         }else if(target > array[guess]){
-          min = guess + 1;   
-         }else{
-             max = guess - 1;
-         } 
-     }   
-     //Didn't find it 
-        return -1;
+    public void sortBinary (int[]array){
+        //keeping track of the position 
+        for (int i = 0; i < array.length; i++) {
+            //go through the rest of the array to look for smaller numbers
+            for (int j = i+1; j < array.length; j++) {
+                //is it smaller
+                if(array[j] < array [i]){
+                    
+                    //swap numbers
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+   
     }
     
+    public int countBinary(int[] binary){
+        //variable to store numbers in 
+        int count = 0;
+       
+        //count+ all the ones
+         for (int i = 0; i < binary.length; i++) {
+             //if there is a "1" in the array, store in the variable
+            if(binary[i] == 1){
+                //keep adding up all the "1s"in the array 
+              count += 1;  
+            }
+         }
+      
+        return count;
+    }
     
     public static void main(String[] args) {
         // Testing Method Above 
-      
-        Question3 test = new Question3();
-         int[] num = {4,8,1,9,7,5,2};
-       
-       
-       
-         System.out.println("After Search:");
-         System.out.println(test.binarySearch(num, 5));
-    }
+         Question3 test = new Question3();
+         //array
+         int[] num = {1,0,0,1,1,1,0,1,0,1};
+         
+         //sorting method
+         test.sortBinary(num);
+         //Print out the sorted array 
+         System.out.println("After Sort");
+         for (int i = 0; i < num.length; i++) {
+             System.out.print(num[i]);
+        }
+         //space
+         System.out.println("");
+        //count binary number method test
+         System.out.println("Total count of \"1s\" in the array: " );
+         System.out.println(test.countBinary(num));
+         
+         
+         
+
+   }
 }
